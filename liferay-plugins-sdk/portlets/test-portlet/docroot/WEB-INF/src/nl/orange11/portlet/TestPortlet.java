12cd2c38a0ea9b8b87da3587ee46034c4f7436cc
Jelmer Kuperus <jelmer@jteam.nl>
2012-05-02 14:55:14 +0200

    Added intellij configuration and test portlet

---
+++ liferay-plugins-sdk/portlets/test-portlet/docroot/WEB-INF/src/nl/orange11/portlet/TestPortlet.java
@@ -0,0 +1,21 @@
+package nl.orange11.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @author Jelmer Kuperus
+ */
+public class TestPortlet extends MVCPortlet {
+
+    @Override
+    public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
+            throws IOException, PortletException {
+        renderRequest.setAttribute("now", new Date());
+    }
+}
